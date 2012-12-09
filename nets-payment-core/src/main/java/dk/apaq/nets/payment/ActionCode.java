bec18e4665a72695f542cf4b8d6bc2e6c580eaa8
michael <michael@michaels-imac.home>
2012-12-09 15:05:56 +0100

    Fixed some code style

--- nets-payment-core/src/main/java/dk/apaq/nets/payment/ActionCode.java
+++ nets-payment-core/src/main/java/dk/apaq/nets/payment/ActionCode.java
@@ -1,59 +1,38 @@
 package dk.apaq.nets.payment;
 
 /**
- *
- * @author michael
+ * ActionCode for Nets Requests.
  */
 public enum ActionCode {
-    Approved("000","Approved","Approved",MerchantAction.Approved),
-    Honour_WithId("001","Honour with identification","Decline",MerchantAction.ContactCustomer),
-    Approved_Partial_Amount("002","Approved for partitial amount","Partly approved",MerchantAction.Try_Again),
-    Approved2("060","Approved","Approved",MerchantAction.Approved),
-    Approved3("061","Approved","Approved",MerchantAction.Approved),
-    Approved4("063","Approved","Approved",MerchantAction.Approved),
-    Do_Not_Honour("100","Do not honour","Decline",MerchantAction.Try_Again),
-    Expired_Card("101","Expired card","Decline/Expired card",MerchantAction.ContactCustomer),
-    Suspected_Fraud("102","Suspected fraud","Decline",MerchantAction.ContactCustomer),
-    Card_Acceptor_Contact_Acquirer("103","Card acceptor contact acquirer","Decline",MerchantAction.ContactCustomer),
-    Restricted_Card("104","Restricted card","Decline",MerchantAction.ContactCustomer),
-    Call_Security("105","Card Acceptor call Acquirers security department","Decline",MerchantAction.ContactCustomer),
-    Pin_Tries_Exceeded("106","Allowable PIN tries exceeded","Decline",MerchantAction.ContactCustomer),
-    Refer_Issuer("107","Refer to card issuer","Decline",MerchantAction.ContactCustomer),
-    Refer_Issuer_Special("108","Refer to card issuer - special conditions","Decline",MerchantAction.ContactCustomer),
-    Invalid_Merchant("109","Invalid merchant","Decline",MerchantAction.Recurring_Not_Allowed),
-    Invalid_Amount("110","Invalid account","Decline/Ammount error",MerchantAction.Data_Error),
-    Invalid_Card_Number("111","Invalid card number","Decline",MerchantAction.ContactCustomer),
-    Pin_Required("112","Pin data required","Decline",MerchantAction.Data_Error),
-    Unacceptable_Fee("113","Unacceptable fee","Decline",MerchantAction.Data_Error),
-    No_Account("114","No account of type request","Decline",MerchantAction.Data_Error),
-    Function_Not_Supported("115","Requested function not supported","Decline",MerchantAction.Data_Error),
-    Insufficient_Funds("116","Insufficient funds","Decline",MerchantAction.Try_Again),
-    Incorrect_Pin("117","Incorrect pin","Decline",null),
-    No_Card_Record("118","No card record","Decline",MerchantAction.ContactCustomer),
-    Not_Permitted_Cardholder("119","Transaction not permitted to cardholder","Decline/Invalid transaction",MerchantAction.ContactCustomer),
-    Not_Permitted_Terminal("120","Transaction not permitted to terminal","Decline/Invalid transaction",MerchantAction.Recurring_Not_Allowed),
-    
-    
-    ;
-/**
-     * ￼￼121 ￼ ￼ ￼ ￼ Exceeds withdrawal amount limit ￼ ￼ ￼ Decline ￼ ￼ ￼ DE
-122 ￼ ￼ ￼ ￼ Security violation ￼ ￼ ￼ Decline ￼ ￼ ￼ DE
-123 ￼ ￼ ￼ ￼ Exceeds withdrawal frequency limit ￼ ￼ ￼ Decline ￼ ￼ ￼ TN
-124 ￼ ￼ ￼ ￼ Violation of law ￼ ￼ ￼ Decline ￼ ￼ ￼ DE
-125 ￼ ￼ ￼ ￼ Card not effective ￼ ￼ ￼ Decline ￼ ￼ ￼ CC
-126 ￼ ￼ ￼ ￼ Invalid pin block ￼ ￼ ￼ Decline
-127 ￼ ￼ ￼ ￼ Pin length error ￼ ￼ ￼ Decline
-128 ￼ ￼ ￼ ￼ Pin key synch error ￼ ￼ ￼ Decline
-129 ￼ ￼ ￼ ￼ Suspected counterfeit card ￼ ￼ ￼ Decline ￼ ￼ ￼ CC
-160 ￼ ￼ ￼ ￼ Invalid date ￼ ￼ ￼ Decline ￼ ￼ ￼ DE
-161 ￼ ￼ ￼ ￼ Allowable number of PIN tries exceeded ￼ ￼ ￼ Decline ￼ ￼ ￼ CC
-162 ￼ ￼ ￼ ￼ Unable to locate previous message ￼ ￼ ￼ Decline ￼ ￼ ￼ DE
-164 ￼ ￼ ￼ ￼ Card entry found, below low range ￼ ￼ ￼ Decline ￼ ￼ ￼ CC
-165 ￼ ￼ ￼ ￼ Pan length not according to table ￼ ￼ ￼ Decline ￼ ￼ ￼ CC
-167 ￼ ￼ ￼ ￼ ￼ Match on previous transaction not ￼ ￼ ￼ Decline ￼ ￼ ￼ ￼ DE
 
-     */
-
+    Approved("000", "Approved", "Approved", MerchantAction.Approved),
+    Honour_WithId("001", "Honour with identification", "Decline", MerchantAction.ContactCustomer),
+    Approved_Partial_Amount("002", "Approved for partitial amount", "Partly approved", MerchantAction.Try_Again),
+    Approved2("060", "Approved", "Approved", MerchantAction.Approved),
+    Approved3("061", "Approved", "Approved", MerchantAction.Approved),
+    Approved4("063", "Approved", "Approved", MerchantAction.Approved),
+    Do_Not_Honour("100", "Do not honour", "Decline", MerchantAction.Try_Again),
+    Expired_Card("101", "Expired card", "Decline/Expired card", MerchantAction.ContactCustomer),
+    Suspected_Fraud("102", "Suspected fraud", "Decline", MerchantAction.ContactCustomer),
+    Card_Acceptor_Contact_Acquirer("103", "Card acceptor contact acquirer", "Decline", MerchantAction.ContactCustomer),
+    Restricted_Card("104", "Restricted card", "Decline", MerchantAction.ContactCustomer),
+    Call_Security("105", "Card Acceptor call Acquirers security department", "Decline", MerchantAction.ContactCustomer),
+    Pin_Tries_Exceeded("106", "Allowable PIN tries exceeded", "Decline", MerchantAction.ContactCustomer),
+    Refer_Issuer("107", "Refer to card issuer", "Decline", MerchantAction.ContactCustomer),
+    Refer_Issuer_Special("108", "Refer to card issuer - special conditions", "Decline", MerchantAction.ContactCustomer),
+    Invalid_Merchant("109", "Invalid merchant", "Decline", MerchantAction.Recurring_Not_Allowed),
+    Invalid_Amount("110", "Invalid account", "Decline/Ammount error", MerchantAction.Data_Error),
+    Invalid_Card_Number("111", "Invalid card number", "Decline", MerchantAction.ContactCustomer),
+    Pin_Required("112", "Pin data required", "Decline", MerchantAction.Data_Error),
+    Unacceptable_Fee("113", "Unacceptable fee", "Decline", MerchantAction.Data_Error),
+    No_Account("114", "No account of type request", "Decline", MerchantAction.Data_Error),
+    Function_Not_Supported("115", "Requested function not supported", "Decline", MerchantAction.Data_Error),
+    Insufficient_Funds("116", "Insufficient funds", "Decline", MerchantAction.Try_Again),
+    Incorrect_Pin("117", "Incorrect pin", "Decline", null),
+    No_Card_Record("118", "No card record", "Decline", MerchantAction.ContactCustomer),
+    Not_Permitted_Cardholder("119", "Transaction not permitted to cardholder", "Decline/Invalid transaction", MerchantAction.ContactCustomer),
+    Not_Permitted_Terminal("120", "Transaction not permitted to terminal", "Decline/Invalid transaction", MerchantAction.Recurring_Not_Allowed),;
+ 
     private String code;
     private String merchantText;
     private String customerText;
@@ -66,30 +45,49 @@ public enum ActionCode {
         this.merchantAction = merchantAction;
     }
 
+    /**
+     * Retrieves the String code.
+     * @return Strig code.
+     */
     public String getCode() {
         return code;
     }
 
+    /**
+     * REtrieves the Merchant Action.
+     * @return The action.
+     */
     public MerchantAction getMerchantAction() {
         return merchantAction;
     }
 
+    /**
+     * A text suitable for Merchants.
+     * @return 
+     */
     public String getMerchantText() {
         return merchantText;
     }
 
+    /**
+     * A text suitable for Customers.
+     * @return The text.
+     */
     public String getCustomerText() {
         return customerText;
     }
-    
+
+    /**
+     * Retrieves an ActionCode from a String code.
+     * @param code The String code.
+     * @return ActionCode matching the String code or null if no matches found. 
+     */
     public static ActionCode fromCode(String code) {
-        for(ActionCode current : ActionCode.values()) {
-            if(code.equals(current.getCode())) {
+        for (ActionCode current : ActionCode.values()) {
+            if (code.equals(current.getCode())) {
                 return current;
             }
         }
         return null;
     }
-    
-    
 }
--- nets-payment-core/src/main/java/dk/apaq/nets/payment/Address.java
+++ nets-payment-core/src/main/java/dk/apaq/nets/payment/Address.java
@@ -1,8 +1,7 @@
 package dk.apaq.nets.payment;
 
 /**
- *
- * @author krog
+ * An Address Entity used for NetRequests.
  */
 public class Address {
     
@@ -11,9 +10,19 @@ public class Address {
     private String city;
     private String countryCode;
 
+    /**
+     * Constructs a new Address.
+     */
     public Address() {
     }
 
+    /**
+     * Constructs a new Address.
+     * @param street The street.
+     * @param postalCode The postlcode of the city.
+     * @param city The name of the city.
+     * @param countryCode The 3-letter countrycode.
+     */
     public Address(String street, String postalCode, String city, String countryCode) {
         this.street = street;
         this.postalCode = postalCode;
@@ -22,35 +31,66 @@ public class Address {
     }
     
     
-
+    /**
+     * Retrieves the city name.
+     * @return The name of the city
+     */
     public String getCity() {
         return city;
     }
 
+    /**
+     * Sets the name of the city.
+     * @param city The name.
+     */
     public void setCity(String city) {
         this.city = city;
     }
 
+    /**
+     * Retrieves the countrycode.
+     * @return The countrycode.
+     */
     public String getCountryCode() {
         return countryCode;
     }
 
+    /**
+     * Sets the countrycode.
+     * @param countryCode 
+     */
     public void setCountryCode(String countryCode) {
         this.countryCode = countryCode;
     }
 
+    /**
+     * Retrieves the postalcode.
+     * @return The postalcode.
+     */
     public String getPostalCode() {
         return postalCode;
     }
 
+    /**
+     * Sets the postalcode.
+     * @param postalCode The postalcode.
+     */
     public void setPostalCode(String postalCode) {
         this.postalCode = postalCode;
     }
 
+    /**
+     * Retrieves the streetname.
+     * @return The streetname
+     */
     public String getStreet() {
         return street;
     }
 
+    /**
+     * Sets the street.
+     * @param street The street.
+     */
     public void setStreet(String street) {
         this.street = street;
     }
